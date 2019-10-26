f9c6312b4af68434570a1383b201c996966fde85
417-72KI <417.72ki@gmail.com>
2019-10-27 01:05:09 +0900

    Bump packages

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -9,14 +9,15 @@ let package = Package(
         .executable(name: "buildconfigswift", targets: ["BuildConfig.swift"])
     ],
     dependencies: [
-        .package(url: "https://github.com/behrang/YamlSwift.git", .upToNextMinor(from: "3.4.3")),
-        .package(url: "https://github.com/jpsim/SourceKitten.git", .upToNextMinor(from: "0.22.0")),
-        .package(url: "https://github.com/kylef/Commander.git", .upToNextMinor(from: "0.8.0")),
-        .package(url: "https://github.com/kylef/PathKit.git", .upToNextMinor(from: "0.9.1")),
-        .package(url: "https://github.com/SwiftGen/StencilSwiftKit.git", .upToNextMinor(from: "2.7.0")),
-        .package(url: "https://github.com/Quick/Nimble.git", .upToNextMinor(from: "8.0.0")),
-        .package(url: "https://github.com/Quick/Quick.git", .upToNextMinor(from: "2.1.0")),
-        .package(url: "https://github.com/Kuniwak/MirrorDiffKit.git", .upToNextMinor(from: "5.0.0")),
+        .package(url: "https://github.com/behrang/YamlSwift.git", .upToNextMajor(from: "3.4.0")),
+        .package(url: "https://github.com/jpsim/SourceKitten.git", .upToNextMajor(from: "0.26.0")),
+        .package(url: "https://github.com/kylef/Commander.git", .upToNextMajor(from: "0.9.0")),
+        .package(url: "https://github.com/kylef/PathKit.git", .upToNextMajor(from: "0.9.0")),
+        // Waiting for updates appling PathKit:1.0.0
+        .package(url: "https://github.com/SwiftGen/StencilSwiftKit.git", .upToNextMajor(from: "2.7.0")),
+        .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "8.0.0")),
+        .package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "2.2.0")),
+        .package(url: "https://github.com/Kuniwak/MirrorDiffKit.git", .upToNextMajor(from: "5.0.0")),
     ],
     targets: [
         .target(
@@ -82,5 +83,5 @@ let package = Package(
             ]
         )
     ],
-    swiftLanguageVersions: [.v4_2]
+    swiftLanguageVersions: [.v5]
 )
