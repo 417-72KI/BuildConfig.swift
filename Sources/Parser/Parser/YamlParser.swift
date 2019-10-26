2d9b0720425b756210b0c0f5d9420cb6e600cea1
417.72KI <417.72ki@gmail.com>
2019-10-26 21:38:49 +0900

    [WIP]

--- Sources/Parser/Parser/YamlParser.swift
+++ Sources/Parser/Parser/YamlParser.swift
@@ -2,7 +2,13 @@ import SourceKittenFramework
 import Yams
 
 struct YamlParser: FileParser {
-    func parse<T>(_ type: T.Type, file: File) throws -> T where T: Decodable {
+    @available(*, deprecated, message: "Use: parse(file:)")
+    func parse<T>(_ type: T.Type, file: File) throws -> T where T : Decodable {
         try YAMLDecoder().decode(T.self, from: file.contents)
+    }
+
+    func parse(file: File) throws -> AnyParsable {
+        (try load(yaml: file.contents))
+            .flatMap(AnyParsable.init) ?? .init()
     }
 }
