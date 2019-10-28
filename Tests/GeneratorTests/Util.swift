0c1b280298429a645dd1bde173af2238fa64bdc3
417-72KI <417.72ki@gmail.com>
2019-10-28 09:58:50 +0900

    Resolve `XCTContext.runActivity(named:block:) failed because activities are disallowed in the current configuration.`

--- Tests/GeneratorTests/Util.swift
+++ Tests/GeneratorTests/Util.swift
@@ -19,7 +19,12 @@ var path: Path {
 }
 
 extension XCTestCase {
-    func context<Result>(_ name: String, block: () throws -> Result) rethrows -> Result {
-        try XCTContext.runActivity(named: name) { _ in try block() }
+    func context(_ name: String, block: () throws -> Void) rethrows {
+        if let _ = ProcessInfo().environment["XCTestConfigurationFilePath"] {
+            // FIXME: Run test with `swift test`, it fails with message: `XCTContext.runActivity(named:block:) failed because activities are disallowed in the current configuration.`
+            return try XCTContext.runActivity(named: name) { _ in try block() }
+        } else {
+            return try block()
+        }
     }
 }
