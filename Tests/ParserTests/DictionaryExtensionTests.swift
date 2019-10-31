fd45403aa4dec58f597c6087dac8f8000573c722
417-72KI <417.72ki@gmail.com>
2019-10-31 12:17:02 +0900

    replace `ParserTests`

--- Tests/ParserTests/DictionaryExtensionTests.swift
+++ Tests/ParserTests/DictionaryExtensionTests.swift
@@ -1,147 +1,138 @@
 import Foundation
-import Quick
-import Nimble
+import XCTest
 
 import Common
 @testable import Parser
 
-final class DictionaryExtensionTests: QuickSpec {
-    override func spec() {
-        describe("Dictionary") {
-            describe("merge") {
-                let json1 = """
-{
-    "a": 1,
-    "b": "2",
-    "c": {
-        "d": 3
-    },
-    "f": 2,
-    "h": {
-        "i": {
-            "j": 9,
-            "k": true,
-            "l": [1, 2, 3]
-        },
-        "n": 0
-    }
-}
-""".data(using: .utf8)!
-                let json2 = """
-{
-    "b": 4,
-    "c": {
-        "e": "5"
-    },
-    "f": {
-        "g": 6
-    },
-    "h": {
-        "i": {
-            "l": [4],
-            "m": 7
-        }
-    }
-}
-""".data(using: .utf8)!
-                let json3 = """
-{
-    "a": 1,
-    "b": 4,
-    "c": {
-        "d": 3,
-        "e": "5"
-    },
-    "f": {
-        "g": 6
-    },
-    "h": {
-        "i": {
-            "j": 9,
-            "k": true,
-            "l": [1, 2, 3, 4],
-            "m": 7
-        },
-        "n": 0
-    }
-}
-""".data(using: .utf8)!
-                do {
-                    let decoder = JSONDecoder()
-                    let dict1 = try decoder.decode(AnyParsable.self, from: json1).value as? [String: AnyParsable] ?? [:]
-                    let dict2 = try decoder.decode(AnyParsable.self, from: json2).value as? [String: AnyParsable] ?? [:]
-                    let dict3 = try decoder.decode(AnyParsable.self, from: json3).value as? [String: AnyParsable] ?? [:]
-                    it("dict1") {
-                        expect { dict1["a"]?.value as? Int }.to(equal(1))
-                        expect { dict1["b"]?.value as? String }.to(equal("2"))
-                        expect { dict1["c"]?.value }.to(beAKindOf([String: AnyParsable].self))
-                        let c = dict1["c"]?.value as? [String: AnyParsable]
-                        expect { c?["d"]?.value as? Int }.to(equal(3))
-                        expect { c?["e"]?.value }.to(beNil())
-                        expect { dict1["f"]?.value as? Int }.to(equal(2))
-                        expect { dict1["h"]?.value }.to(beAKindOf([String: AnyParsable].self))
-                        let h = dict1["h"]?.value as? [String: AnyParsable]
-                        expect { h?["i"]?.value }.to(beAKindOf([String: AnyParsable].self))
-                        let i = h?["i"]?.value as? [String: AnyParsable]
-                        expect { i?["j"]?.value as? Int }.to(equal(9))
-                        expect { i?["k"]?.value as? Bool }.to(beTrue())
-                        expect { i?["l"]?.value }.to(beAKindOf([AnyParsable].self))
-                        let l = i?["l"]?.value as? [AnyParsable]
-                        expect { l?.map { $0.value as? Int } }.to(equal([1, 2, 3]))
-                        expect { i?["m"]?.value }.to(beNil())
-                        expect { h?["n"]?.value as? Int }.to(equal(0))
-                    }
-                    it("dict2") {
-                        expect { dict2["a"]?.value }.to(beNil())
-                        expect { dict2["b"]?.value as? Int }.to(equal(4))
-                        expect { dict2["c"]?.value }.to(beAKindOf([String: AnyParsable].self))
-                        let c = dict2["c"]?.value as? [String: AnyParsable]
-                        expect { c?["d"]?.value }.to(beNil())
-                        expect { c?["e"]?.value as? String }.to(equal("5"))
-                        expect { dict2["f"]?.value }.to(beAKindOf([String: AnyParsable].self))
-                        let f = dict2["f"]?.value as? [String: AnyParsable]
-                        expect { f?["g"]?.value as? Int }.to(equal(6))
-                        expect { dict2["h"]?.value }.to(beAKindOf([String: AnyParsable].self))
-                        let h = dict2["h"]?.value as? [String: AnyParsable]
-                        expect { h?["i"]?.value }.to(beAKindOf([String: AnyParsable].self))
-                        let i = h?["i"]?.value as? [String: AnyParsable]
-                        expect { i?["j"]?.value }.to(beNil())
-                        expect { i?["k"]?.value }.to(beNil())
-                        expect { i?["l"]?.value }.to(beAKindOf([AnyParsable].self))
-                        let l = i?["l"]?.value as? [AnyParsable]
-                        expect { l?.map { $0.value as? Int } }.to(equal([4]))
-                        expect { i?["m"]?.value as? Int }.to(equal(7))
-                        expect { h?["n"]?.value }.to(beNil())
-                    }
-                    it("dict3") {
-                        expect { dict3["a"]?.value as? Int }.to(equal(1))
-                        expect { dict3["b"]?.value as? Int }.to(equal(4))
-                        expect { dict3["c"]?.value }.to(beAKindOf([String: AnyParsable].self))
-                        let c = dict3["c"]?.value as? [String: AnyParsable]
-                        expect { c?["d"]?.value as? Int }.to(equal(3))
-                        expect { c?["e"]?.value as? String }.to(equal("5"))
-                        expect { dict3["f"]?.value }.to(beAKindOf([String: AnyParsable].self))
-                        let f = dict3["f"]?.value as? [String: AnyParsable]
-                        expect { f?["g"]?.value as? Int }.to(equal(6))
-                        expect { dict3["h"]?.value }.to(beAKindOf([String: AnyParsable].self))
-                        let h = dict3["h"]?.value as? [String: AnyParsable]
-                        expect { h?["i"]?.value }.to(beAKindOf([String: AnyParsable].self))
-                        let i = h?["i"]?.value as? [String: AnyParsable]
-                        expect { i?["j"]?.value as? Int }.to(equal(9))
-                        expect { i?["k"]?.value as? Bool }.to(beTrue())
-                        expect { i?["l"]?.value }.to(beAKindOf([AnyParsable].self))
-                        let l = i?["l"]?.value as? [AnyParsable]
-                        expect { l?.map { $0.value as? Int } }.to(equal([1, 2, 3, 4]))
-                        expect { i?["m"]?.value as? Int }.to(equal(7))
-                        expect { h?["n"]?.value as? Int }.to(equal(0))
-                    }
-                    it("dict1 + dict2 equal dict3") {
-                        expect { AnyParsable(dict1 + dict2) }.to(equal(AnyParsable(dict3)))
-                    }
-                } catch {
-                    fail(error.localizedDescription)
+final class DictionaryExtensionTests: XCTestCase {
+    func testDictionary() throws {
+        let json1 = """
+            {
+                "a": 1,
+                "b": "2",
+                "c": {
+                    "d": 3
+                },
+                "f": 2,
+                "h": {
+                    "i": {
+                        "j": 9,
+                        "k": true,
+                        "l": [1, 2, 3]
+                    },
+                    "n": 0
                 }
             }
+            """.data(using: .utf8)!
+        let json2 = """
+            {
+                "b": 4,
+                "c": {
+                    "e": "5"
+                },
+                "f": {
+                    "g": 6
+                },
+                "h": {
+                    "i": {
+                        "l": [4],
+                        "m": 7
+                    }
+                }
+            }
+            """.data(using: .utf8)!
+        let json3 = """
+            {
+                "a": 1,
+                "b": 4,
+                "c": {
+                    "d": 3,
+                    "e": "5"
+                },
+                "f": {
+                    "g": 6
+                },
+                "h": {
+                    "i": {
+                        "j": 9,
+                        "k": true,
+                        "l": [1, 2, 3, 4],
+                        "m": 7
+                    },
+                    "n": 0
+                }
+            }
+            """.data(using: .utf8)!
+
+        let decoder = JSONDecoder()
+        let dict1 = try decoder.decode(AnyParsable.self, from: json1).value as? [String: AnyParsable] ?? [:]
+        let dict2 = try decoder.decode(AnyParsable.self, from: json2).value as? [String: AnyParsable] ?? [:]
+        let dict3 = try decoder.decode(AnyParsable.self, from: json3).value as? [String: AnyParsable] ?? [:]
+        try context("dict1") {
+            XCTAssertEqual(dict1["a"]?.value as? Int, 1)
+            XCTAssertEqual(dict1["b"]?.value as? String, "2")
+
+            let c = try XCTUnwrap(dict1["c"]?.value as? [String: AnyParsable])
+            XCTAssertEqual(c["d"]?.value as? Int, 3)
+            XCTAssertNil(c["e"]?.value)
+            XCTAssertEqual(dict1["f"]?.value as? Int, 2)
+
+            let h = try XCTUnwrap(dict1["h"]?.value as? [String: AnyParsable])
+            let i = try XCTUnwrap(h["i"]?.value as? [String: AnyParsable])
+            XCTAssertEqual(i["j"]?.value as? Int, 9)
+            XCTAssertEqual(i["k"]?.value as? Bool, true)
+
+            let l = try XCTUnwrap(i["l"]?.value as? [AnyParsable])
+            XCTAssertEqual(l.map { $0.value as? Int }, [1, 2, 3])
+            XCTAssertNil(i["m"]?.value)
+            XCTAssertEqual(h["n"]?.value as? Int, 0)
+        }
+        try context("dict2") {
+            XCTAssertNil(dict2["a"]?.value)
+            XCTAssertEqual(dict2["b"]?.value as? Int, 4)
+
+            let c = try XCTUnwrap(dict2["c"]?.value as? [String: AnyParsable])
+            XCTAssertNil(c["d"]?.value)
+            XCTAssertEqual(c["e"]?.value as? String, "5")
+
+            let f = try XCTUnwrap(dict2["f"]?.value as? [String: AnyParsable])
+            XCTAssertEqual(f["g"]?.value as? Int, 6)
+
+            let h = try XCTUnwrap(dict2["h"]?.value as? [String: AnyParsable])
+
+            let i = try XCTUnwrap(h["i"]?.value as? [String: AnyParsable])
+            XCTAssertNil(i["j"]?.value)
+            XCTAssertNil(i["k"]?.value)
+
+            let l = try XCTUnwrap(i["l"]?.value as? [AnyParsable])
+            XCTAssertEqual(l.map { $0.value as? Int }, [4])
+            XCTAssertEqual(i["m"]?.value as? Int, 7)
+            XCTAssertNil(h["n"]?.value)
+        }
+        try context("dict3") {
+            XCTAssertEqual(dict3["a"]?.value as? Int, 1)
+            XCTAssertEqual(dict3["b"]?.value as? Int, 4)
+
+            let c = try XCTUnwrap(dict3["c"]?.value as? [String: AnyParsable])
+            XCTAssertEqual(c["d"]?.value as? Int, 3)
+            XCTAssertEqual(c["e"]?.value as? String, "5")
+
+            let f = try XCTUnwrap(dict3["f"]?.value as? [String: AnyParsable])
+            XCTAssertEqual(f["g"]?.value as? Int, 6)
+
+            let h = try XCTUnwrap(dict3["h"]?.value as? [String: AnyParsable])
+
+            let i = try XCTUnwrap(h["i"]?.value as? [String: AnyParsable])
+            XCTAssertEqual(i["j"]?.value as? Int, 9)
+            XCTAssertEqual(i["k"]?.value as? Bool, true)
+
+            let l = try XCTUnwrap(i["l"]?.value as? [AnyParsable])
+            XCTAssertEqual(l.map { $0.value as? Int }, [1, 2, 3, 4])
+            XCTAssertEqual(i["m"]?.value as? Int, 7)
+            XCTAssertEqual(h["n"]?.value as? Int, 0)
+        }
+        context("dict1 + dict2 equal dict3") {
+            XCTAssertEqual(AnyParsable(dict1 + dict2), AnyParsable(dict3))
         }
     }
 }
--- Tests/ParserTests/FileParserTests.swift
+++ Tests/ParserTests/FileParserTests.swift
@@ -1,142 +1,84 @@
 import Foundation
 import PathKit
 import SourceKittenFramework
-
-import Quick
-import Nimble
-
 import Common
+import XCTest
+
 @testable import Parser
 
-final class FileParserTests: QuickSpec {
-    override func spec() {
-        describe("YamlParser") {
-            let parser = YamlParser()
-            context("valid yaml") {
-                let f = File(path: path + Path("test.yaml"))
-                it("found") {
-                    expect { f }.notTo(beNil())
-                }
-                guard let file = f else { return }
-                context("for TestContent") {
-                    it("success") {
-                        let expected = TestContent(
-                            api: TestContent.API(
-                                domain: URL(string: "http://localhost")!,
-                                api: [
-                                    "login": TestContent.API.Path(method: .post, path: "/login"),
-                                    "getList": TestContent.API.Path(method: .get, path: "/list")
-                                ],
-                                timeout: 11.5
-                            ),
-                            count: 0,
-                            isDebug: true
-                        )
-                        expect { try parser.parse(TestContent.self, file: file) }.to(equal(expected))
-                    }
-                }
-                context("for AnyParsable") {
-                    do {
-                        let data = try parser.parse(file: file)
-                        context("root") {
-                            expect { data.value }.to(beAKindOf([String: AnyParsable].self))
-                            guard let root = data.value as? [String: AnyParsable] else { return }
-                            it("API") {
-                                expect(root.keys).to(contain("API"))
-                                expect { root["API"]?.value }.to(beAKindOf([String: AnyParsable].self))
-                                guard let api = root["API"]?.value as? [String: AnyParsable] else { return }
-                                expect(api.keys).to(contain("domain"))
-                                expect { api["domain"]?.value }.to(beAKindOf(String.self))
-                                guard let domain = api["domain"]?.value as? String else { return }
-                                expect { domain }.to(equal("http://localhost"))
+final class FileParserTests: XCTestCase {
+    func testYamlParser() throws {
+        let parser = YamlParser()
+        try context("valid yaml") {
+            let file = try XCTUnwrap(File(path: path + Path("test.yaml")))
+            try context("for TestContent") {
+                let expected = TestContent(
+                    api: TestContent.API(
+                        domain: URL(string: "http://localhost")!,
+                        api: [
+                            "login": TestContent.API.Path(method: .post, path: "/login"),
+                            "getList": TestContent.API.Path(method: .get, path: "/list")
+                        ],
+                        timeout: 11.5
+                    ),
+                    count: 0,
+                    isDebug: true
+                )
+                XCTAssertEqual(try parser.parse(TestContent.self, file: file), expected)
+            }
+            try context("for AnyParsable") {
+                let data = try parser.parse(file: file)
+                let root = try XCTUnwrap(data.value as? [String: AnyParsable])
+                let api = try XCTUnwrap(root["API"]?.value as? [String: AnyParsable])
+                let domain = try XCTUnwrap(api["domain"]?.value as? String)
+                XCTAssertEqual(domain, "http://localhost")
 
-                                expect(api.keys).to(contain("timeout"))
-                                expect { api["timeout"]?.value }.to(beAKindOf(Double.self))
-                                guard let timeout = api["timeout"]?.value as? Double else { return }
-                                expect { timeout }.to(equal(11.5))
-                            }
-                            it("count") {
-                                expect(root.keys).to(contain("count"))
-                                expect { root["count"]?.value }.to(beAKindOf(Int.self))
-                                guard let count = root["count"]?.value as? Int else { return }
-                                expect { count }.to(equal(0))
-                            }
-                            it("is_debug") {
-                                expect(root.keys).to(contain("is_debug"))
-                                expect { root["is_debug"]?.value }.to(beAKindOf(Bool.self))
-                                guard let isDebug = root["is_debug"]?.value as? Bool else { return }
-                                expect(isDebug).to(beTrue())
-                            }
-                        }
-                    } catch {
-                        fail(error.localizedDescription)
+                let timeout = try XCTUnwrap(api["timeout"]?.value as? Double)
+                XCTAssertEqual(timeout, 11.5, accuracy: 0.1)
 
-                    }
-                }
+                let count = try XCTUnwrap(root["count"]?.value as? Int)
+                XCTAssertEqual(count, 0)
+
+                let isDebug = try XCTUnwrap(root["is_debug"]?.value as? Bool)
+                XCTAssertTrue(isDebug)
             }
         }
-        describe("JSonParser") {
-            let parser = JSonParser()
-            context("valid json") {
-                let f = File(path: path + Path("test.json"))
-                it("found") {
-                    expect { f }.notTo(beNil())
-                }
-                guard let file = f else { return }
-                context("for TestContent") {
-                    it("success") {
-                        let expected = TestContent(
-                            api: TestContent.API(
-                                domain: URL(string: "http://localhost")!,
-                                api: [
-                                    "login": TestContent.API.Path(method: .post, path: "/login"),
-                                    "getList": TestContent.API.Path(method: .get, path: "/list")
-                                ],
-                                timeout: 10.5
-                            ),
-                            count: 0,
-                            isDebug: true
-                            )
-                        expect { try parser.parse(TestContent.self, file: file) }.to(equal(expected))
-                    }
-                }
-                context("for AnyParsable") {
-                    do {
-                        let data = try parser.parse(file: file)
-                        context("root") {
-                            expect { data.value }.to(beAKindOf([String: AnyParsable].self))
-                            guard let root = data.value as? [String: AnyParsable] else { return }
-                            it("API") {
-                                expect(root.keys).to(contain("API"))
-                                expect { root["API"]?.value }.to(beAKindOf([String: AnyParsable].self))
-                                guard let api = root["API"]?.value as? [String: AnyParsable] else { return }
-                                expect(api.keys).to(contain("domain"))
-                                expect { api["domain"]?.value }.to(beAKindOf(String.self))
-                                guard let domain = api["domain"]?.value as? String else { return }
-                                expect { domain }.to(equal("http://localhost"))
+    }
 
-                                expect(api.keys).to(contain("timeout"))
-                                expect { api["timeout"]?.value }.to(beAKindOf(Double.self))
-                                guard let timeout = api["timeout"]?.value as? Double else { return }
-                                expect { timeout }.to(equal(10.5))
-                            }
-                            it("count") {
-                                expect(root.keys).to(contain("count"))
-                                expect { root["count"]?.value }.to(beAKindOf(Int.self))
-                                guard let count = root["count"]?.value as? Int else { return }
-                                expect { count }.to(equal(0))
-                            }
-                            it("is_debug") {
-                                expect(root.keys).to(contain("is_debug"))
-                                expect { root["is_debug"]?.value }.to(beAKindOf(Bool.self))
-                                guard let isDebug = root["is_debug"]?.value as? Bool else { return }
-                                expect(isDebug).to(beTrue())
-                            }
-                        }
-                    } catch {
-                        fail(error.localizedDescription)
-                    }
-                }
+    func testJSonParser() throws {
+        let parser = JSonParser()
+        try context("valid json") {
+            let file = try XCTUnwrap(File(path: path + Path("test.json")))
+            try context("for TestContent") {
+                let expected = TestContent(
+                    api: TestContent.API(
+                        domain: URL(string: "http://localhost")!,
+                        api: [
+                            "login": TestContent.API.Path(method: .post, path: "/login"),
+                            "getList": TestContent.API.Path(method: .get, path: "/list")
+                        ],
+                        timeout: 10.5
+                    ),
+                    count: 0,
+                    isDebug: true
+                )
+                XCTAssertEqual(try parser.parse(TestContent.self, file: file), expected)
+            }
+            try context("for AnyParsable") {
+                let data = try parser.parse(file: file)
+                let root = try XCTUnwrap(data.value as? [String: AnyParsable])
+                let api = try XCTUnwrap(root["API"]?.value as? [String: AnyParsable])
+                let domain = try XCTUnwrap(api["domain"]?.value as? String)
+                XCTAssertEqual(domain, "http://localhost")
+                
+                let timeout = try XCTUnwrap(api["timeout"]?.value as? Double)
+                XCTAssertEqual(timeout, 10.5, accuracy: 0.1)
+                
+                let count = try XCTUnwrap(root["count"]?.value as? Int)
+                XCTAssertEqual(count, 0)
+                
+                let isDebug = try XCTUnwrap(root["is_debug"]?.value as? Bool)
+                XCTAssertTrue(isDebug)
             }
         }
     }
