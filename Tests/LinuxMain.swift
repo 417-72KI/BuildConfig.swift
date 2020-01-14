84518a24bda931da49a82e4ebf5f38bee258051b
417-72KI <417.72ki@gmail.com>
2020-01-14 14:32:11 +0900

    Fix LinuxMain

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,7 +1,22 @@
 import XCTest
+import Quick
 
-import ConfigurationPlistTests
+@testable import BuildConfig_swiftTests
+@testable import GeneratorTests
+@testable import ParserTests
 
-var tests = [XCTestCaseEntry]()
-tests += ConfigurationPlistTests.allTests()
-XCTMain(tests)
+//var tests = [XCTestCaseEntry]()
+////tests += BuildConfigswiftTests.allTests
+//tests += GeneratorTests.allTests
+////tests += DictionaryExtensionTests.allTests
+//XCTMain(tests)
+
+Quick.QCKMain([
+    GeneratorTests.self,
+    DictionaryExtensionTests.self
+    ],
+testCases: [
+    testCase(GeneratorTests.allTests),
+    testCase(DictionaryExtensionTests.allTests),
+    ]
+)
