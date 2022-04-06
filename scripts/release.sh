4d0a021d9a00332e68fe80de944309d557d8a4cf
417-72KI <417.72ki@gmail.com>
2022-04-06 19:15:47 +0900

    remove build steps from release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -32,40 +32,21 @@ if ! type "gsed" > /dev/null; then
     brew install gnu-sed
 fi
 
-if ! type "github-release" > /dev/null; then
-    echo '`github-release` not found. Install'
-    go get github.com/aktau/github-release
+if ! type "gh" > /dev/null; then
+    echo '`gh` not found. Install'
+    brew install gh
 fi
 
 cd $(git rev-parse --show-toplevel)
 
-# Build
-rm -f .build/${EXECUTABLE_NAME}.zip
-swift build -c release -Xswiftc -suppress-warnings
-.build/release/${EXECUTABLE_NAME} --version
-zip -j .build/${EXECUTABLE_NAME}.zip .build/release/${EXECUTABLE_NAME} LICENSE
-
 # Version
-TAG=$(cat "${APPLICATION_INFO_FILE}" | grep version | awk '{ print $NF }' | gsed -r 's/\"(.*)\"/\1/g')
+TAG="$(swift run $EXECUTABLE_NAME --version 2>/dev/null)"
 gsed -i -r "s/(s\.version\s*?=\s)\"([0-9]*\.[0-9]*\.[0-9]*?)\"/\1\"${TAG}\"/g" ${PROJECT_NAME}.podspec
 git commit -m "Bump version to ${TAG}" "${PROJECT_NAME}.podspec" "${APPLICATION_INFO_FILE}"
 
-# TAG
-git tag "${TAG}"
-git push origin master "${TAG}"
+# # TAG
+# git tag "${TAG}"
+# git push origin master "${TAG}"
 
 # GitHub Release
-github-release release \
-    --user 417-72KI \
-    --repo ${PROJECT_NAME} \
-    --tag "${TAG}"
-
-github-release upload \
-    --user 417-72KI \
-    --repo ${PROJECT_NAME} \
-    --tag "${TAG}" \
-    --name "${EXECUTABLE_NAME}.zip" \
-    --file .build/${EXECUTABLE_NAME}.zip
-
-# CocoaPods
-bundle exec pod trunk push ${PROJECT_NAME}.podspec
+gh release create "$TAG"
