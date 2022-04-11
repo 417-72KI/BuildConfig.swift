de88ed64bfce32f57fbd1529a13c224b178687ad
417-72KI <417.72ki@gmail.com>
2022-04-11 15:46:52 +0900

    fix lint on AppDelegate

--- Demo/BuildConfigSwiftDemo/Sources/UI/AppDelegate.swift
+++ Demo/BuildConfigSwiftDemo/Sources/UI/AppDelegate.swift
@@ -10,9 +10,9 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     var window: UIWindow?
 
+    // swiftlint:disable:next discouraged_optional_collection
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         #if DEBUG
         if isTesting {
