70c19f49931da159f22300cf7b18f73af8a570ed
417-72KI <417.72ki@gmail.com>
2022-04-11 18:25:59 +0900

    fix context

--- Tests/ParserTests/Util.swift
+++ Tests/ParserTests/Util.swift
@@ -10,11 +10,12 @@ var path: Path {
 
 extension XCTestCase {
     func context(_ name: String, block: () throws -> Void) rethrows {
+        #if os(macOS)
         if let _ = ProcessInfo().environment["XCTestConfigurationFilePath"] {
             // FIXME: Run test with `swift test`, it fails with message: `XCTContext.runActivity(named:block:) failed because activities are disallowed in the current configuration.`
             return try XCTContext.runActivity(named: name) { _ in try block() }
-        } else {
-            return try block()
         }
+        #endif
+        return try block()
     }
 }
