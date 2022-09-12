8b40e93c67e15e40a48b8e6165b14b9826d2f992
417-72KI <417.72ki@gmail.com>
2022-09-13 00:33:46 +0900

    requires Swift 5.6 and macOS Monterey

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "BuildConfig.swift",
-    platforms: [ .macOS(.v11) ],
+    platforms: [ .macOS(.v12) ],
     products: [
         .executable(name: "buildconfigswift", targets: ["BuildConfig.swift"])
     ],
