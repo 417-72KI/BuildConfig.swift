5d526176b90903386a83fbd39de4c875764a5ee8
417-72KI <417.72ki@gmail.com>
2022-10-26 22:53:51 +0900

    dumping BuildConfig.plist is no longer needed

--- Sources/Common/Constants.swift
+++ Sources/Common/Constants.swift
@@ -1,5 +1,4 @@
 public enum Constants {
-    public static let defaultOutputFileName = "BuildConfig.plist"
     public static let generatedSwiftFileName = "BuildConfig.generated.swift"
 
     public static let lastRunFileName = "buildconfigswift-lastrun"
