7d11cf72eb52784f51b30199e70a6cda17545f3f
417-72KI <417.72ki@gmail.com>
2023-10-05 00:26:10 +0900

    split test jobs

---
+++ scripts/test.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -o pipefail
+
+XCRESULT_PATH='test_output/BuildConfig.swift.xcresult'
+
+if [[ -e "$XCRESULT_PATH" ]]; then
+    rm -rf "$XCRESULT_PATH"
+fi
+
+SCHEME="$(xcrun --sdk macosx xcodebuild -list -json | jq -rc '.workspace.schemes[] | select(. | startswith("BuildConfig"))')"
+
+echo "Scheme: $SCHEME"
+
+xcrun --sdk macosx xcodebuild \
+    -enableCodeCoverage YES \
+    -scheme "${SCHEME}" \
+    -destination "platform=macOS" \
+    -clonedSourcePackagesDirPath '.build/SourcePackages' \
+    -resultBundlePath "$XCRESULT_PATH" \
+    clean test | xcpretty
