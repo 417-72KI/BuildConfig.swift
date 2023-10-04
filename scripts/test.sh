2c0db0234eb3212c75f9052a6fd6a59730a38e82
417-72KI <417.72ki@gmail.com>
2023-10-05 01:04:14 +0900

    extract commands to shell scripts

---
+++ scripts/test.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -o pipefail
+
+XCRESULT_PATH='test_output/BuildConfig.swift.xcresult'
+
+if [[ -e "$XCRESULT_PATH" ]]; then
+    rm -rf "$XCRESULT_PATH"
+fi
+
+SCHEME="$(xcrun --sdk macosx xcodebuild -list -json | jq -rc '.workspace.schemes[] | select(. | startswith("BuildConfig"))')"
+
+xcrun --sdk macosx xcodebuild \
+    -enableCodeCoverage YES \
+    -scheme "${SCHEME}" \
+    -destination "platform=macOS" \
+    -derivedDataPath 'DerivedData' \
+    -clonedSourcePackagesDirPath '.build/SourcePackages' \
+    -resultBundlePath "$XCRESULT_PATH" \
+    clean test | xcpretty
---
+++ scripts/test_demo.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -o pipefail
+
+XCRESULT_PATH='test_output/BuildConfigDemo.xcresult'
+
+if [[ -e "$XCRESULT_PATH" ]]; then
+    rm -rf "$XCRESULT_PATH"
+fi
+
+if [[ "$(find Demo -depth 1 -name '*.xcodeproj')" == '' ]]; then
+    $(dirname $0)/copy-lint-config.sh
+    xcrun --sdk macosx swift run --package-path Demo/Tools xcodegen --spec Demo/project.yml 
+fi
+
+PROJECT_PATH=$(find Demo -depth 1 -name '*.xcodeproj' | head -n 1)
+SCHEME="$(xcrun --sdk macosx xcodebuild -project "${PROJECT_PATH}" -list -json | jq -rc '.project.schemes[] | select(. | contains("Demo"))')"
+xcrun --sdk macosx xcodebuild \
+    -skipPackagePluginValidation \
+    -enableCodeCoverage YES \
+    -project "${PROJECT_PATH}" \
+    -scheme "${SCHEME}" \
+    -destination 'platform=iOS Simulator,name=iPhone 14 Pro' \
+    -derivedDataPath 'Demo/DerivedData' \
+    -clonedSourcePackagesDirPath 'Demo/.build/SourcePackages' \
+    -resultBundlePath "$XCRESULT_PATH" \
+    clean test | xcpretty
