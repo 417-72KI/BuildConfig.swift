f02f189c76c678909c4d62f2648380a01265857e
417-72KI <417.72ki@gmail.com>
2023-10-05 10:20:36 +0900

    Revert to develop mode

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 
 import PackageDescription
 
-let isDevelop = false
+let isDevelop = true
 
 let devDependencies: [Package.Dependency] = isDevelop ? [
     .package(url: "https://github.com/realm/SwiftLint.git", from: "0.53.0"),
