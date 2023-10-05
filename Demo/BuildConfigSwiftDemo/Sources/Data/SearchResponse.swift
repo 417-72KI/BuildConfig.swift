281b4c3e24519f8e955e3bde16aae98957ca8907
417-72KI <417.72ki@gmail.com>
2023-10-05 23:06:32 +0900

    create fake API client for demo

---
+++ Demo/BuildConfigSwiftDemo/Sources/Data/SearchResponse.swift
@@ -0,0 +1,12 @@
+//
+//  SearchResponse.swift
+//  BuildConfigSwiftDemo
+//
+//  Created by 417.72KI on 2023/10/05.
+//  Copyright © 2023 417.72KI. All rights reserved.
+//
+
+import Foundation
+
+struct SearchResponse: Response {
+}
