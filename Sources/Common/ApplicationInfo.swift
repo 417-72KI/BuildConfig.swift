1a412f272d88fe745271c3959f485e967e4aaa4f
417-72KI <417.72ki@gmail.com>
2023-10-14 22:56:35 +0900

    Bump version to 6.0.0-alpha.12

--- Sources/Common/ApplicationInfo.swift
+++ Sources/Common/ApplicationInfo.swift
@@ -1,4 +1,4 @@
 public enum ApplicationInfo {
     public static let name = "BuildConfig.swift"
-    public static let version = "6.0.0-alpha.11"
+    public static let version = "6.0.0-alpha.12"
 }
